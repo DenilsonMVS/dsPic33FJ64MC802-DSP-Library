6a10d28e93ec7a96dfe1b4f33501b7accdf1c6aa
Denilson Martins Vieira Santos <denilsonmvs@gmail.com>
2021-03-04 18:06:36 -0300

    Update round.c

--- round.c
+++ round.c
@@ -2,10 +2,10 @@
 #include "round.h"
 
 long roundFloat(float num) {
-    if(num >= 0)
-        return num + 0.5;
+    if(num >= 0.0f)
+        return num + 0.5f;
     else
-        return num - 0.5;
+        return num - 0.5f;
 }
 
 
