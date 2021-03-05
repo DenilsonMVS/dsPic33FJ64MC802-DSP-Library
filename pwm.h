9fe85ee47520bc89aa88517be7e243f178a89d3d
Denilson Martins Vieira Santos <denilsonmvs@gmail.com>
2021-03-04 21:30:45 -0300

    Update pwm.h

--- pwm.h
+++ pwm.h
@@ -2,6 +2,10 @@
 #ifndef PWM_H
 #define PWM_H
 
+// Habilita pwm no módulo do PWM2. 
+// Parâmetro "value" deve ser de 0 a 1000. 
+// Parâmetro pin deve ser 1, 2 ou 3. Referindo-se ao módulo 1, 2 e 3 do PWM1.
+// No caso, o sinal do pwm será enviado para o pino alto (H). O pino baixo (L) estará em 0V.
 extern void pwm(int value, char pin);
 
 #endif
